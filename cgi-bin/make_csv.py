7e7ed02e668823b6662ed49bb57143614d6231ee
nosscafe66 <nosscafe66@gmail.com>
2023-11-21 23:05:15 +0900

    csvファイル出力変更

--- cgi-bin/make_csv.py
+++ cgi-bin/make_csv.py
@@ -6,52 +6,51 @@ __version__ = "0.0.1"
 __date__    = "22 Janualy 2022"
 __prodactname__ = "Get_CSV"
 
-import cgi, cgitb, sqlite3, codecs
+import cgi
+import cgitb
+import sqlite3
 import csv
+import io
 import datetime
+
 cgitb.enable()
 
-print("Content-Type: text/html")
-print()
-form = cgi.FieldStorage()
-form_check = 0
-
 DB_NAME = 'TeamK.db'
-conn = sqlite3.connect(DB_NAME)
-cur = conn.cursor()
 
-account_table_list=cur.execute('SELECT * FROM task_tbl')
-account_table_list = cur.fetchall()
+def make_csv_content(account_table_list):
+    output = io.StringIO()
+    writer = csv.writer(output, quoting=csv.QUOTE_NONE, escapechar='\\')
 
-now = datetime.datetime.now()
-filename = './csv/account_list_' + now.strftime('%Y%m%d_%H%M%S') + '.csv'
+    # データベースの内容をCSVとして書き込む
+    header = ["TaskID", "Task", "DueDate", "MailAddress", "CreatedAt", "Time"]
+    writer.writerow(header)
+    for data in account_table_list:
+        now_str = datetime.datetime.now().strftime('%H:%M:%S.%f')
+        writer.writerow(list(data) + [now_str])
 
-def make_file(account_table_list:list) -> None:
-    header = ["TaskID", "Task", "DueDate", "MailAddress", "CreatedAt","Time"]
-    with open(filename, 'a', newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_NONE, escapechar='\\')
-        writer.writerow(header)
-        for data in account_table_list:
-            now_str = datetime.datetime.now().strftime('%H:%M:%S.%f')
-            writer.writerow(list(data) + [now_str])
-
-def download_csvfile(csvfile:str) -> str:
-    afterpage = codecs.open('./afterpage/DataUnload.html', 'r', 'utf-8').read()
-    afterpage = afterpage.replace('{% filename %}', str(csvfile))
-    # conn.commit()
-    #cur.close()
-    #conn.close()
-    return afterpage
+    return output.getvalue()
 
 def main():
     try:
-      csvfile=make_file(account_table_list)
-      afterpage=download_csvfile(csvfile)
-      print(afterpage)
-      cur.close()
-      conn.close()
-    except Exception as exceptmessage:
-      print(exceptmessage)
+        # データベースからデータを取得
+        conn = sqlite3.connect(DB_NAME)
+        cur = conn.cursor()
+        account_table_list = cur.execute('SELECT * FROM task_tbl').fetchall()
+        cur.close()
+        conn.close()
 
-if __name__=="__main__":
+        csv_content = make_csv_content(account_table_list)
+
+        # CSVファイルをダウンロードするためのHTTPヘッダー
+        print("Content-Type: text/csv")
+        print("Content-Disposition: attachment; filename=\"account_list.csv\"")
+        print()
+        print(csv_content)
+
+    except Exception as e:
+        print("Content-Type: text/html")
+        print()
+        print(f"<html><body><p>Error: {e}</p></body></html>")
+
+if __name__ == "__main__":
     main()
